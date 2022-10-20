344981c0b4cdaad35f1283aaf3ec13cc60f72909
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-10-20 13:27:23 +0900

    #S07P31B102-57 init: SpringBoot 개발환경 설정

---
+++ Back/Selly/src/main/java/com/nct/Selly/SellyApplication.java
@@ -0,0 +1,13 @@
+package com.nct.Selly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellyApplication.class, args);
+	}
+
+}
