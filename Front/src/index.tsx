695a4724bb54f0f2101aaa86b14a24c0b8a4999c
s_infinite <s_infinite@naver.com>
2022-10-25 17:53:01 +0900

    #init/S07P31B102-201 fix: index.tsx 수정 & @reduxjs/toolkit, react-redux 설치

--- Front/src/index.tsx
+++ Front/src/index.tsx
@@ -1,6 +1,12 @@
-import { StrictMode, createElement } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const reactDomRoot = createRoot(document.getElementById("root"));
-reactDomRoot.render(createElement(StrictMode, null, createElement(App)));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
