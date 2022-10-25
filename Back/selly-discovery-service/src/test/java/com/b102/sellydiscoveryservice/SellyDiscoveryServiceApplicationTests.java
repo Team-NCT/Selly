5b81061f216273eaa2b8182307ed3e2756e9fbb5
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-10-25 17:44:58 +0900

    #S07P31B102-143 init: eureka server, api gateway, user-service 세팅

---
+++ Back/selly-discovery-service/src/test/java/com/b102/sellydiscoveryservice/SellyDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b102.sellydiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellyDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
