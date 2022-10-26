1acf34fea4b338b3d0f3151c5063221a65b7362f
JK <worudp12@gmail.com>
2022-10-27 01:44:51 +0900

    #S07P31B102-181 feat: 리덕스 기본 환경 설정 및 테스트 counter 로직 구현

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home, Counter } from "./pages";
+import { Home, Counter } from "@/pages";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* 메인 페이지 */}
         <Route path="/" element={<Home />} />
-        <Route path="counter" element={<Counter />} />
+
+        {/* 테스트 페이지 */}
+        <Route path="/counter" element={<Counter />} />
       </Routes>
     </>
   );
--- Front/src/index.tsx
+++ Front/src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/styles.css";
+import "@/styles/styles.css";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "@/redux/store";
+import store from "@/redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
