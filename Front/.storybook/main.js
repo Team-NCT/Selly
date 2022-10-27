a16e9440d3c86a3d3c82521252745ddd4a3d5057
s_infinite <s_infinite@naver.com>
2022-10-27 13:49:49 +0900

    #feat/S07P31B102-202 feat. storybook 설정

--- Front/.storybook/main.js
+++ Front/.storybook/main.js
@@ -1,3 +1,4 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,9 +6,16 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-sass-postcss",
+    "storybook-addon-react-router-v6",
+    "@storybook/addon-interactions",
+    "@storybook/addon-controls",
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
   },
 };
