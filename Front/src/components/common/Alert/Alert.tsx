81e3a32f717aa1dc5e77b2915506be4c6ae90f9c
ji01217 <remi1217@naver.com>
2022-10-28 14:30:41 +0900

    S07P31B102-180 feat. combinereducer 추가

--- Front/src/components/common/Alert/Alert.tsx
+++ Front/src/components/common/Alert/Alert.tsx
@@ -2,7 +2,7 @@
 import { AlertProps } from "./Alert.types";
 // import styles from "./Alert.module.scss";
 
-const Alert = ({ state = false, styles = "success", icon = "none", message = "" }: AlertProps) => {
+const Alert = ({ state = true, styles = "success", icon = "none", message = "" }: AlertProps) => {
   // const {closeAlert} =
   return <dialog open={state}>hihi</dialog>;
 };
--- Front/src/redux/store.ts
+++ Front/src/redux/store.ts
@@ -1,13 +1,14 @@
+import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
 //! reducer import
 import counter from "./slices/counterSlice";
 import alert from "@/redux/slices/alertSlice";
 
-const reducers = {
+const reducers = combineReducers({
   counter,
   alert,
-};
+});
 
 const store = configureStore({
   reducer: reducers,
