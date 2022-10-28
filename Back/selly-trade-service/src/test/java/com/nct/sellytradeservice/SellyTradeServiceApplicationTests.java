b8969ec5e89f666454b4201e419e08f2188d5e6b
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-10-28 17:58:10 +0900

    #S07P31B102-171 feat.거래서비스 생성

---
+++ Back/selly-trade-service/src/test/java/com/nct/sellytradeservice/SellyTradeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nct.sellytradeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellyTradeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
