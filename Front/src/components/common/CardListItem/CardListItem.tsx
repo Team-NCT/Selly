fc7d33635cf20f85dfa56c4e682bb433f0f340d8
JK <worudp12@gmail.com>
2022-10-29 20:38:52 +0900

    #S07P31B102-195 feat. 카드 리스트 컴포넌트 구현

--- Front/src/components/common/CardListItem/CardListItem.tsx
+++ Front/src/components/common/CardListItem/CardListItem.tsx
@@ -2,7 +2,10 @@ import { CardListItemProps } from "./CardListItem.types";
 import style from "./CardListItem.module.scss";
 import { Card } from "@/components/common";
 
-const CardListItem = ({ url, title, href, supply }: CardListItemProps) => {
+const CardListItem = ({ url, title, id, supply }: CardListItemProps) => {
+  //TODO_JK: 상세 페이지 링크가 나오면 href 수정 (id를 받아서 링크로 수정)
+  const href = `/${id}`;
+
   return (
     <li className={style.card_list_item}>
       <a href={href}>
--- Front/src/components/common/CardListItem/CardListItem.types.ts
+++ Front/src/components/common/CardListItem/CardListItem.types.ts
@@ -2,7 +2,7 @@ import { CardProps } from "@/components/common/Card/Card.types";
 
 export interface CardListItemProps extends CardProps {
   /**
-   * 클릭 시, 이동할 url (required)
+   * 클릭 시, 이동할 url 정보를 위한 NFT id (required)
    */
-  href: string;
+  id: string;
 }
