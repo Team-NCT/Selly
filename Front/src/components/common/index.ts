9d27706cc361deeedf0ef57dec7c99c82a38190c
JK <worudp12@gmail.com>
2022-10-30 00:47:40 +0900

    #S07P31B102-195 feat. NFT 설명 카드 리스트 컴포넌트 구현

--- Front/src/components/common/index.ts
+++ Front/src/components/common/index.ts
@@ -1,3 +1,6 @@
 export { default as Card } from "./Card/Card";
 export { default as CardListItem } from "./CardListItem/CardListItem";
+export { default as CardList } from "./CardList/CardList";
 export { default as DescCard } from "./DescCard/DescCard";
+export { default as DescCardListItem } from "./DescCardListItem/DescCardListItem";
+export { default as DescCardList } from "./DescCardList/DescCardList";
