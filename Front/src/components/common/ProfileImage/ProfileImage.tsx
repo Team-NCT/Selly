8a1fdb6b5b434558e78991650a4e86f621a1b882
JK <worudp12@gmail.com>
2022-10-30 19:30:55 +0900

    #S07P31B102-193 feat. 프로필 이미지 컴포넌트 구현

---
+++ Front/src/components/common/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,13 @@
+import style from "./ProfileImage.module.scss";
+import { ProfileImageProps } from "./ProfileImage.types";
+import defaultImage from "@/assets/images/profile.png";
+
+const ProfileImage = ({ url, size, profileStyle }: ProfileImageProps) => {
+  return (
+    <img
+      src={url ? url : defaultImage}
+      alt="user-profile"
+      className={`${style[`size_${size}`]} ${style[`profile_${profileStyle}`]}`}></img>
+  );
+};
+export default ProfileImage;
