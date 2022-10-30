b2bab78abc8afb7f0e3e7fc721277cb59c92eb2f
jiyoung <remi1217@naver.com>
2022-10-30 23:24:05 +0900

    #S07P31B102-177 feat.네비게이션 틀 구현

--- Front/src/components/common/index.ts
+++ Front/src/components/common/index.ts
@@ -7,3 +7,5 @@ export { default as DescCardListItem } from "./DescCardListItem/DescCardListItem
 export { default as DescCardList } from "./DescCardList/DescCardList";
 export { default as Alert } from "./Alert/Alert";
 export { default as Navbar } from "./Navbar/Navbar";
+export { default as TextInput } from "./TextInput/TextInput";
+export { default as ProfileImage } from "./ProfileImage/ProfileImage";
