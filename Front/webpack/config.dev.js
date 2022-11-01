fd282335649382652bd344176aede6963d779de9
황상윤 <s_infinite@naver.com>
2022-11-02 01:51:19 +0900

    #S07P31B102-282 fix. build 오류 해결

--- Front/webpack/config.dev.js
+++ Front/webpack/config.dev.js
@@ -26,13 +26,11 @@ const devConfig = {
   ],
   module: {
     rules: [
-      // jsx
+      // typescript
       {
-        test: /\.jsx?$/i,
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: "babel-loader",
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
       },
 
       // scsss
@@ -64,12 +62,6 @@ const devConfig = {
             },
           },
         ],
-      },
-      // typescript
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
       },
 
       // asset
