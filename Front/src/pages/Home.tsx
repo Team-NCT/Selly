f737806eeb44d015fe8dbb4335762e95b71488bd
s_infinite <s_infinite@naver.com>
2022-11-02 17:54:24 +0900

    #S07P31B102-207 feat. 로그인 로직 redux 연결

--- Front/src/pages/Home.tsx
+++ Front/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useAppSelector } from "@/hooks/useStore";
+import { selectAccount } from "@/store/loginSlice";
 
 function Home() {
-  return <h1>Home</h1>;
+  const { account } = useAppSelector(selectAccount);
+  return (
+    <>
+      <h1>Home</h1>
+      <h1>account 주소: {account}</h1>
+    </>
+  );
 }
 
 export default Home;
--- Front/src/store/index.ts
+++ Front/src/store/index.ts
@@ -3,3 +3,4 @@ export { default as counter } from "./counterSlice";
 
 export { default as alert } from "./alertSlice";
 export { default as modal } from "./modalSlice";
+export { default as account } from "./loginSlice";
