98f1e6cb74596fca614d322e8037e5da7251655d
ji01217 <remi1217@naver.com>
2022-11-02 16:16:46 +0900

    #S07P31B102-184 fix.conflict 수정

--- Front/src/hooks/index.ts
+++ Front/src/hooks/index.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-export { useAlert } from "./useAlert";
-export { useInputState } from "./useInputState";
-=======
 export * from "./useStore";
 export * from "./useAlert";
 
 export { default as useInputState } from "./useInputState";
 export { default as useKakaoShare } from "./useKakaoShare";
->>>>>>> cb8522aab2c44a1b1bd3f8ecd865ffbd60856e1d
