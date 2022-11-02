f16f77abcfbfbbff3fb9fe9a137f5cf9eda52227
박재경 <worudp12@gmail.com>
2022-11-02 13:27:46 +0900

    #S07P31B102-199 feat. NFT 상세 페이지 헤더 UI 구현 완료

--- Front/src/components/NFTDetail/Header/NFTDetailHeader/NFTDetailHeader.stories.tsx
+++ Front/src/components/NFTDetail/Header/NFTDetailHeader/NFTDetailHeader.stories.tsx
@@ -14,7 +14,7 @@ export default {
 export const Default: Story<NFTDetailHeaderProps> = (args) => <NFTDetailHeader {...args} />;
 
 Default.args = {
-  title: "좀비와 함께 춤을",
+  title: "좀비와 함께 춤을좀비와 함께 춤을좀비와 함께 춤을좀비와 함께 춤을",
   imageUrl:
     "https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
   favoriteStatus: true,
