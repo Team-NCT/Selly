acf5b892622b5bebfb0063a84968076b50da7465
jiyoung <remi1217@naver.com>
2022-11-06 23:43:28 +0900

    #S07P31B102-211 feat. 유효성 검사 추가

--- Front/src/components/create/Form/components/Link/Link.tsx
+++ Front/src/components/create/Form/components/Link/Link.tsx
@@ -2,9 +2,30 @@ import { Label, TextInput } from "@/components/common";
 import { useInputState } from "@/hooks";
 import style from "./Link.module.scss";
 import form_style from "../../Form.module.scss";
+import { useEffect, useState } from "react";
+import { checkUrl } from "@/helpers/utils/checkUrl";
+import { LinkProps } from "../../Form.types";
 
-const Link = () => {
+const Link = ({ setIsLinkTrue }: LinkProps) => {
   const [value, handleInputChange] = useInputState();
+  //* value가 빈 값이거나 url형식이 맞다면 true, 형식이 맞지 않다면 false
+  const [isTrue, setIsTrue] = useState<boolean>(true);
+  useEffect(() => {
+    const debounce = setTimeout(async () => {
+      setIsTrue(false);
+      if (value.trim().length === 0) {
+        setIsTrue(true);
+      } else {
+        setIsTrue(checkUrl(value));
+      }
+    }, 200);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [value]);
+  useEffect(() => {
+    setIsLinkTrue(isTrue);
+  }, [isTrue, setIsLinkTrue]);
   return (
     <div className={form_style.form_item}>
       <Label
@@ -21,8 +42,9 @@ const Link = () => {
         value={value}
         handleInputChange={handleInputChange}
         maxLength={100}
-        status={true}
-        placeHolder="해당 작품에 연결하고 싶은 사이트 링크를 입력해주세요."></TextInput>
+        status={isTrue}
+        placeHolder="해당 작품에 연결하고 싶은 사이트 링크를 입력해주세요."
+        errorMessage="Link는 http:// 혹은 https://로 시작해야 합니다."></TextInput>
       <p className={style.input_desc}>최대 100글자까지 입력 가능합니다.</p>
     </div>
   );
--- Front/src/components/create/Form/components/Title/Title.tsx
+++ Front/src/components/create/Form/components/Title/Title.tsx
@@ -2,9 +2,27 @@ import style from "./Title.module.scss";
 import form_style from "../../Form.module.scss";
 import { Label, TextInput } from "@/components/common";
 import { useInputState } from "@/hooks";
+import { useEffect, useState } from "react";
+import { TitleProps } from "../../Form.types";
 
-const Title = () => {
+const Title = ({ setIsTitleTrue }: TitleProps) => {
   const [value, handleInputChange] = useInputState();
+  //* value가 빈 값이면 false
+  const [isTrue, setIsTrue] = useState<boolean>(true);
+  useEffect(() => {
+    const debounce = setTimeout(async () => {
+      setIsTrue(false);
+      if (value.trim().length === 0) {
+        setIsTrue(false);
+      } else {
+        setIsTrue(true);
+        setIsTitleTrue(true);
+      }
+    }, 200);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [setIsTitleTrue, value]);
   return (
     <div className={form_style.form_item}>
       <Label color="sherbet" positionH="bottom" positionW="right" id="create-title">
@@ -16,8 +34,9 @@ const Title = () => {
         value={value}
         handleInputChange={handleInputChange}
         maxLength={20}
-        status={true}
-        placeHolder="작품명을 입력해주세요."></TextInput>
+        status={isTrue}
+        placeHolder="작품명을 입력해주세요."
+        errorMessage="작품명은 필수 항목입니다."></TextInput>
       <p className={style.input_desc}>최대 20글자까지 입력 가능합니다.</p>
     </div>
   );
