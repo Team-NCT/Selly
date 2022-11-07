9b9b4a98e025adda7884bfbef0e9a50c651f7c46
ji01217 <remi1217@naver.com>
2022-11-07 14:47:00 +0900

    #S07P31B102-241 feat. Create 페이지 리팩토링

--- Front/src/components/create/Form/components/Title/Title.tsx
+++ Front/src/components/create/Form/components/Title/Title.tsx
@@ -2,27 +2,33 @@ import style from "./Title.module.scss";
 import form_style from "../../Form.module.scss";
 import { Label, TextInput } from "@/components/common";
 import { useInputState } from "@/hooks";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { TitleProps } from "../../Form.types";
 
 const Title = ({ setIsTitleTrue }: TitleProps) => {
-  const [value, handleInputChange] = useInputState();
-  //* value가 빈 값이면 false
-  const [isTrue, setIsTrue] = useState<boolean>(true);
+  //* input, button state
+  const [inputStatus, setInputStatus] = useState(true);
+  const [buttonStatus, setButtonStatus] = useState(false);
+
+  //* button의 state가 바뀔때마다 부모에 올려줌.
   useEffect(() => {
-    const debounce = setTimeout(async () => {
-      setIsTrue(false);
-      if (value.trim().length === 0) {
-        setIsTrue(false);
-      } else {
-        setIsTrue(true);
-        setIsTitleTrue(true);
-      }
-    }, 200);
-    return () => {
-      clearTimeout(debounce);
-    };
-  }, [setIsTitleTrue, value]);
+    setIsTitleTrue(buttonStatus);
+  }, [buttonStatus, setIsTitleTrue]);
+
+  //* input의 길이에 따라 input과 button의 state를 바꾸는 함수
+  const isTitleTrue = useCallback((value: string) => {
+    if (value.trim().length == 0) {
+      setInputStatus(false);
+      setButtonStatus(false);
+      return value;
+    } else {
+      setInputStatus(true);
+      setButtonStatus(true);
+      return value;
+    }
+  }, []);
+
+  const [value, handleInputChange] = useInputState("", isTitleTrue);
   return (
     <div className={form_style.form_item}>
       <Label color="sherbet" positionH="bottom" positionW="right" id="create-title">
@@ -30,14 +36,16 @@ const Title = ({ setIsTitleTrue }: TitleProps) => {
       </Label>
       <span className={style.caption_danger}>*</span>
       <p className={style.input_desc}>최대 20글자까지 입력 가능합니다.</p>
-      <TextInput
-        id="create-title"
-        value={value}
-        handleInputChange={handleInputChange}
-        maxLength={20}
-        status={isTrue}
-        placeHolder="작품명을 입력해주세요."
-        errorMessage="작품명은 필수 항목입니다."></TextInput>
+      <div className={style.input}>
+        <TextInput
+          id="create-title"
+          value={value}
+          handleInputChange={handleInputChange}
+          maxLength={20}
+          status={inputStatus}
+          placeHolder="작품명을 입력해주세요."
+          errorMessage="작품명은 필수 항목입니다."></TextInput>
+      </div>
     </div>
   );
 };
