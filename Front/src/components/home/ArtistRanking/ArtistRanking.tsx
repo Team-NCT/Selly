e28cef9979a735067e7d92f98ff20a20eab7158c
ji01217 <remi1217@naver.com>
2022-11-08 10:21:40 +0900

    #S07P31B102-241 feat. 랭킹 아이템 폴더 이름 수정

---
+++ Front/src/components/home/ArtistRanking/ArtistRanking.tsx
@@ -0,0 +1,11 @@
+import ArtistRankingList from "./ArtistRankingList/ArtistRankingList";
+
+const ArtistRanking = () => {
+  return (
+    <div>
+      <ArtistRankingList></ArtistRankingList>
+    </div>
+  );
+};
+
+export default ArtistRanking;
--- Front/src/components/home/ArtistRanking/ArtistRankingITem/ArtistRankingItem.tsx
+++ Front/src/components/home/ArtistRanking/ArtistRankingITem/ArtistRankingItem.tsx
@@ -1,26 +1,29 @@
 import { ArtistRankingProps } from "./AritstRankingItem.type";
 import style from "./ArtistRankingItem.module.scss";
 import { ProfileImage } from "@/components/common";
+import { Link } from "react-router-dom";
 
 const ArtistRankingItem = (props: ArtistRankingProps) => {
   return (
-    <section className={style.item_container}>
-      <div className={style.item_artist_section}>
-        <div className={style.item_rank}>{props.rank}</div>
-        <div className={style.item_artist}>
-          <ProfileImage size="xs" profileStyle="square" url={props.image} />
-          <div className={style.item_artist_info}>
-            <span className={style.item_artist_nickname}>{props.nickname}</span>
-            {props.isAuth && <span>✓</span>}
-            <div className={style.item_artist_wallet}>{props.wallet}</div>
+    <Link to="/profile">
+      <section className={style.item_container}>
+        <div className={style.item_artist_section}>
+          <div className={style.item_rank}>{props.rank}</div>
+          <div className={style.item_artist}>
+            <ProfileImage size="xs" profileStyle="square" url={props.image} />
+            <div className={style.item_artist_info}>
+              <span className={style.item_artist_nickname}>{props.nickname}</span>
+              {props.isAuth && <span>✓</span>}
+              <div className={style.item_artist_wallet}>{props.wallet}</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style.item_cnt_section}>
-        <div className={style.item_artist_cnt}>{props.followerCnt}</div>
-        <div className={style.item_artist_cnt}>{props.NFTCnt}</div>
-      </div>
-    </section>
+        <div className={style.item_cnt_section}>
+          <div className={style.item_artist_cnt}>{props.followerCnt}</div>
+          <div className={style.item_artist_cnt}>{props.NFTCnt}</div>
+        </div>
+      </section>
+    </Link>
   );
 };
 
