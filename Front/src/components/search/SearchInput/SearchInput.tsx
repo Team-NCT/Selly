00041df34717477f14442a81f565d4999a39772a
박재경 <worudp12@gmail.com>
2022-11-08 12:46:44 +0900

    #S07P31B102-318 feat. 검색 인풋 컴포넌트 구현 완료

--- Front/src/components/search/SearchInput/SearchInput.tsx
+++ Front/src/components/search/SearchInput/SearchInput.tsx
@@ -1,12 +1,17 @@
 import { SearchIcon } from "@/components/icon";
+import { FormEvent } from "react";
 import style from "./SearchInput.module.scss";
 
 const SearchInput = () => {
+  const submitSearchForm = (event: FormEvent) => {
+    event.preventDefault();
+    alert("검색");
+  };
   return (
-    <div className={style.search_input}>
-      <input type="text" placeholder="작품, 작가명을 검색하세요" />
+    <form className={style.search_input} onSubmit={submitSearchForm}>
+      <input type="search" placeholder="작품, 작가명을 검색하세요" />
       <SearchIcon />
-    </div>
+    </form>
   );
 };
 
