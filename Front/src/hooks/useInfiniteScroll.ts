f2ca7d678e0aad1a93e86bef39a90df154905d60
imukyee <imukyee@gmail.com>
2022-11-09 13:31:49 +0900

    #feat/S07P31B102-303 feat. useInfiniteScroll 훅 제작

--- Front/src/hooks/useInfiniteScroll.ts
+++ Front/src/hooks/useInfiniteScroll.ts
@@ -3,33 +3,30 @@ import { throttle } from "lodash";
 
 const THROTTLE_WAIT = 300;
 
-export default function useInfiniteScroll(fetchCallback: any) {
+const useInfiniteScroll = (callback: () => void) => {
   const [isFetching, setIsFetching] = useState(false);
 
-  const handleScrollThrottle = throttle(() => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop >=
-      document.documentElement.offsetHeight
-    ) {
-      setIsFetching(true);
-    }
-  }, THROTTLE_WAIT);
-
   useEffect(() => {
-    window.addEventListener("scroll", handleScrollThrottle);
-
-    return () => {
-      window.removeEventListener("scroll", handleScrollThrottle);
-    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
   }, []);
 
   useEffect(() => {
-    if (!isFetching) {
-      return;
-    }
-    fetchCallback();
-    setIsFetching(false);
+    if (!isFetching) return;
+    callback();
   }, [isFetching]);
 
+  const scrollHandler = throttle(() => {
+    const currentScroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const bodyHeight = document.body.clientHeight;
+    const paddingBottom = 50;
+
+    if (currentScroll + windowHeight + paddingBottom < bodyHeight) return;
+    setIsFetching(true);
+  }, THROTTLE_WAIT);
+
   return [isFetching, setIsFetching];
-}
+};
+
+export default useInfiniteScroll;
