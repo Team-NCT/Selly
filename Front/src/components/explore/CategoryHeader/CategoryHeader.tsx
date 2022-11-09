e0069e8185cc2bf1ad0be40affd724bc11609c84
ji01217 <remi1217@naver.com>
2022-11-09 16:04:01 +0900

    #S07P31B102-285 feat. Explore 헤더 제작

--- Front/src/components/explore/CategoryHeader/CategoryHeader.tsx
+++ Front/src/components/explore/CategoryHeader/CategoryHeader.tsx
@@ -3,6 +3,21 @@ import { CategoryHeaderProps } from "./CategoryHeader.types";
 import styles from "./CategoryHeader.module.scss";
 
 const CategoryHeader = ({ category }: CategoryHeaderProps) => {
+  let title = "";
+  switch (category) {
+    case "all":
+      title = "ALL NFTs";
+      break;
+    case "digital":
+      title = "Digital";
+      break;
+    case "analog":
+      title = "Analog";
+      break;
+    case "photography":
+      title = "Photography";
+      break;
+  }
   return (
     <header className={styles.header}>
       <div className={styles.header_title}>
@@ -13,7 +28,7 @@ const CategoryHeader = ({ category }: CategoryHeaderProps) => {
           width={70}
           vertical={1}
           horizontal={2}>
-          {category}
+          {title}
         </Neon>
       </div>
     </header>
--- Front/src/pages/Explore/Category.tsx
+++ Front/src/pages/Explore/Category.tsx
@@ -3,24 +3,9 @@ import { useParams } from "react-router-dom";
 
 const Category = () => {
   const { category } = useParams();
-  let title = "";
-  switch (category) {
-    case "all":
-      title = "ALL NFTs";
-      break;
-    case "digital":
-      title = "Digital";
-      break;
-    case "analog":
-      title = "Analog";
-      break;
-    case "photography":
-      title = "Photography";
-      break;
-  }
   return (
     <main>
-      <CategoryHeader category={title} />
+      <CategoryHeader category={category as string} />
     </main>
   );
 };
