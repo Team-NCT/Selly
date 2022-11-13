db6e581e22a1ef89741d9de97e75283352ecf471
박성배 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-11-14 00:45:59 +0900

    #S07P31B102-341 feat. NFT 상세정보 구현

---
+++ Back/selly-article-service/src/main/java/com/nct/sellyarticleservice/domain/dto/ResponseInfoArticle.java
@@ -0,0 +1,14 @@
+package com.nct.sellyarticleservice.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseInfoArticle {
+  private String tokenId;
+  private String ownershipContractAddress;
+  private String metaDataUrl;
+  private String articleImgUrl;
+  private String contractAddress;
+}
--- Back/selly-trade-service/src/main/java/com/nct/sellytradeservice/domain/dto/SellRegistRequest.java
+++ Back/selly-trade-service/src/main/java/com/nct/sellytradeservice/domain/dto/SellRegistRequest.java
@@ -1,5 +1,6 @@
 package com.nct.sellytradeservice.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.nct.sellytradeservice.domain.entity.TradeLog;
 import com.nct.sellytradeservice.domain.entity.TradeRegist;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SellRegistRequest {
   private Long seller;
 
