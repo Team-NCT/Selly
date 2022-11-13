0a7826298487a9f2ef22b3baa2aed5767fed645b
jiyoung <remi1217@naver.com>
2022-11-14 00:23:24 +0900

    #S07P3B102-297 feat. lazyquery => query로 수정

--- Front/src/components/explore/ExploreCardList/ExploreCardList.tsx
+++ Front/src/components/explore/ExploreCardList/ExploreCardList.tsx
@@ -1,44 +1,22 @@
-import { useLazyFetchNFTListQuery } from "@/api/server/exploreAPI/exploreAPI";
+import { useFetchNFTListQuery } from "@/api/server/exploreAPI/exploreAPI";
 import { DescCardList } from "@/components/common";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ExploreParamsType } from "./ExploreCardList.types";
 import style from "./ExploreCardList.module.scss";
-import { NFTDescCardDataType } from "@/types/NFTData.types";
 
 const ExploreCardList = ({ category, sort, order }: ExploreParamsType) => {
-  const [fetchNFTList] = useLazyFetchNFTListQuery();
-
-  //* fetch된 cardlist data를 저장하는 변수
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<NFTDescCardDataType[]>([]);
-
-  //@ description: cardList를 반환하는 함수
-  const requestSortNFTList = useCallback(
-    async (params: ExploreParamsType) => {
-      try {
-        const { data } = await fetchNFTList(params);
-        if (!data) return;
-        setData(data);
-        setIsLoading(false);
-        console.log(data);
-      } catch (err) {
-        console.error(err);
-        //@ TodoJY: 404 페이지로 이동
-      }
-    },
-    [fetchNFTList]
-  );
+  const { data, isError } = useFetchNFTListQuery({ category, sort, order });
 
   useEffect(() => {
-    console.log("따란");
-    requestSortNFTList({ category, sort, order });
-  }, [category, order, requestSortNFTList, sort]);
+    if (!isError) return;
+    alert("404로 이동"), [isError];
+  }, [isError]);
 
   return (
     <section className={style.container}>
-      {isLoading ? (
+      {!data ? (
         "loading"
-      ) : data.length === 0 ? (
+      ) : data?.length === 0 ? (
         <div className={style.no_data}>현재 조건에 맞는 판매 등록된 NFT가 없어요!</div>
       ) : (
         <DescCardList data={data} />
