a216bfc36f26cf13861953e374b17672dd572ebf
imukyee <imukyee@gmail.com>
2022-11-16 02:30:40 +0900

    #S07P31B102-228 feat. 금고 생성 이벤트 제거

--- Blockchain/contracts/FractionalizedNFTFactory.sol
+++ Blockchain/contracts/FractionalizedNFTFactory.sol
@@ -4,57 +4,56 @@ pragma solidity ^0.8.4;
 import "./FractionalizedNFT.sol";
 
 contract FractionalizedNFTFactory {
-  // 분할된 NFT들의 컨트랙트 주소를 저장하는 배열
-  address[] public fractionalizedNFTCAs;
-  // 해당 NFT의 CA에 토큰 id를 넣으면 금고 컨트랙트(FractionalizedNFT)의 주소가 나오도록 매필
-  mapping(address => mapping(uint256 => address)) public F_NFTCA;
-  
-  // 이벤트
-  event FractionalizeNFT(
-    address F_CA,
-    address NFTCA,
-    uint256 tokenId,
-    address owner,
-    uint256 amount
-  );
+    // 분할된 NFT들의 컨트랙트 주소를 저장하는 배열
+    address[] public fractionalizedNFTCAs;
+    // 해당 NFT의 CA에 토큰 id를 넣으면 금고 컨트랙트(FractionalizedNFT)의 주소가 나오도록 매필
+    mapping(address => mapping(uint256 => address)) public F_NFTCA;
 
-  constructor() {}
-
-  function Fractionalize 
-  (
-    address _NFTCA, 
-    uint256 _tokenId, 
-    uint256 _amount, 
-    string memory _name, 
-    string memory _symbol
-  ) 
-    external 
-    returns (address)
-  {
-    address fractionalizedNFTCA = address(
-        new FractionalizedNFT(_NFTCA, _tokenId, _amount, msg.sender, _name, _symbol)
+    // 이벤트
+    event FractionalizeNFT(
+        address F_CA,
+        address NFTCA,
+        uint256 tokenId,
+        address owner,
+        uint256 amount
     );
-    fractionalizedNFTCAs.push(fractionalizedNFTCA);
-    F_NFTCA[_NFTCA][_tokenId] = fractionalizedNFTCA;
 
-    emit FractionalizeNFT(fractionalizedNFTCA, _NFTCA, _tokenId, msg.sender, _amount);
+    constructor() {}
 
-    return fractionalizedNFTCA;
-  }
+    function Fractionalize(
+        address _NFTCA,
+        uint256 _tokenId,
+        uint256 _amount,
+        string memory _name,
+        string memory _symbol
+    ) external returns (address) {
+        address fractionalizedNFTCA = address(
+            new FractionalizedNFT(
+                _NFTCA,
+                _tokenId,
+                _amount,
+                msg.sender,
+                _name,
+                _symbol
+            )
+        );
+        fractionalizedNFTCAs.push(fractionalizedNFTCA);
+        F_NFTCA[_NFTCA][_tokenId] = fractionalizedNFTCA;
 
-  function allF_NFTCAs() public view returns (address[] memory) {
-    return fractionalizedNFTCAs;
-  }
+        // emit FractionalizeNFT(fractionalizedNFTCA, _NFTCA, _tokenId, msg.sender, _amount);
 
-  function CAOfNFTCAbyTokenId
-  (
-    address _NFTCA, 
-    uint256 _tokenId
-  ) 
-    public 
-    view 
-    returns (address) 
-  {
-    return F_NFTCA[_NFTCA][_tokenId];
-  }
+        return fractionalizedNFTCA;
+    }
+
+    function allF_NFTCAs() public view returns (address[] memory) {
+        return fractionalizedNFTCAs;
+    }
+
+    function CAOfNFTCAbyTokenId(address _NFTCA, uint256 _tokenId)
+        public
+        view
+        returns (address)
+    {
+        return F_NFTCA[_NFTCA][_tokenId];
+    }
 }
