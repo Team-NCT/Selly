afd3de5f7e5f20c32d96724581477efb2ff143e4
jiyoung <remi1217@naver.com>
2022-11-15 23:27:41 +0900

    #S07P3B102-425 feat. Explore 페이지 무한스크롤 구현

--- Front/src/components/explore/ExploreCardList/ExploreCardList.tsx
+++ Front/src/components/explore/ExploreCardList/ExploreCardList.tsx
@@ -1,17 +1,33 @@
 import { useFetchNFTListQuery } from "@/api/server/exploreAPI/exploreAPI";
 import { DescCardList, Spinner } from "@/components/common";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ExploreParamsType } from "./ExploreCardList.types";
 import style from "./ExploreCardList.module.scss";
 import { useNavigate } from "react-router-dom";
+import { useInfiniteScroll } from "@/hooks";
+import { NFTDescCardDataType } from "@/types/NFTData.types";
 
 const CATEGORY = ["all", "digital", "analog", "photography"];
 const SORT = ["asc", "desc"];
 const ORDER = ["sellRegist", "price"];
+const SIZE = 12;
 
 const ExploreCardList = ({ category, sort, order }: ExploreParamsType) => {
-  const { data } = useFetchNFTListQuery({ category, sort, order });
+  const { data, isLoading } = useFetchNFTListQuery({ category, sort, order });
+  const [currentData, setCurrentData] = useState<NFTDescCardDataType[]>([]);
+  const [page, setPage] = useState(1);
   const navigate = useNavigate();
+
+  //* 인피니티 스크롤 연결
+  useInfiniteScroll(() => setPage((prev) => prev + 1));
+
+  //* 페이지가 증가하면 데이터를 더 불러온다.
+  useEffect(() => {
+    if (!data) return;
+    const currentPage = page * SIZE;
+    const exploreResultData = data.slice(0, currentPage);
+    setCurrentData([...exploreResultData]);
+  }, [data, page]);
 
   useEffect(() => {
     if (CATEGORY.includes(category) && SORT.includes(sort) && ORDER.includes(order)) return;
@@ -20,15 +36,15 @@ const ExploreCardList = ({ category, sort, order }: ExploreParamsType) => {
 
   return (
     <section className={style.container}>
-      {!data ? (
+      {isLoading ? (
         <div className={style.loading}>
           <Spinner />
           Loading...
         </div>
-      ) : data?.length === 0 ? (
+      ) : currentData?.length === 0 ? (
         <div className={style.no_data}>현재 조건에 맞는 판매 등록된 NFT가 없어요!</div>
       ) : (
-        <DescCardList data={data} />
+        <DescCardList data={currentData} />
       )}
     </section>
   );
